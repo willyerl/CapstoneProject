0bf11587c974a37c892a20fa8157490ee390f047
willyerl <115436181+willyerl@users.noreply.github.com>
2023-01-10 11:32:23 +1300

    Update FullRecipe.js

--- backEnd/middleware/FullRecipe.js
+++ backEnd/middleware/FullRecipe.js
@@ -7,10 +7,10 @@ async function fullRecipe(id, res) {
     if(id !== "undefined"){
         
     let recipes = 
-    await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=ea79b9c31eda4feeb4a5976bfb1c3c6e&includeNutrition=false`)
-    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=f290e9cc36c94b1fa0ae203e0f51e638&includeNutrition=false`)
-    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=6b1f93497871425fa94f02b64771e1af&includeNutrition=false`)
-    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=cd676049cb274e2ebefca3a8594e8d86&includeNutrition=false`)
+    await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey="apiKey"&includeNutrition=false`)
+    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey="apiKey"&includeNutrition=false`)
+    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey="apiKey"&includeNutrition=false`)
+    // await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey="apiKey"&includeNutrition=false`)
     
     let data = await recipes.json()
     for(let i of data.extendedIngredients){
